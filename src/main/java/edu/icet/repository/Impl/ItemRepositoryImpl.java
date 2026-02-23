7911f1a667557eac402830823967285fa8c97dc1
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-23 23:51:15 +0530

    create customer commit #22

--- src/main/java/edu/icet/repository/Impl/ItemRepositoryImpl.java
+++ src/main/java/edu/icet/repository/Impl/ItemRepositoryImpl.java
@@ -3,15 +3,30 @@ package edu.icet.repository.Impl;
 import edu.icet.model.dto.ItemDTO;
 import edu.icet.repository.ItemRepository;
 import edu.icet.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
+@RequiredArgsConstructor
 public class ItemRepositoryImpl implements ItemRepository {
 
+    final private JdbcTemplate jdbcTemplate;
 
     @Override
     public boolean addItem(ItemDTO item) {
-        return false;
+
+        String sql = "INSERT INTO item VALUE(?,?,?,?,?)";
+
+        return jdbcTemplate.update(sql,
+                item.getItemCode(),
+                item.getDescription(),
+                item.getPackSize(),
+                item.getUnitPrice(),
+                item.getQtyOnHand()
+                )>0;
     }
 
     @Override
