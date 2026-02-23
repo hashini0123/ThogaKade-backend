8a972ba248562664c9a68fa1d330943b3688ba72
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-23 22:30:36 +0530

    create customer commit #21

--- src/main/java/edu/icet/service/Impl/ItemServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/ItemServiceImpl.java
@@ -1,4 +1,28 @@
 package edu.icet.service.Impl;
 
-public class ItemServiceImpl {
+import edu.icet.model.dto.ItemDTO;
+import edu.icet.service.ItemService;
+
+import java.util.List;
+
+public class ItemServiceImpl implements ItemService {
+    @Override
+    public boolean addItem(ItemDTO item) {
+        return false;
+    }
+
+    @Override
+    public boolean updateItem(ItemDTO item) {
+        return false;
+    }
+
+    @Override
+    public boolean deleteItem(Integer id) {
+        return false;
+    }
+
+    @Override
+    public List<ItemDTO> getAll() {
+        return List.of();
+    }
 }
