82a71b489c9c8e4de05dde85f7a4380df4e1e1c5
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-23 22:30:23 +0530

    create customer commit #20

--- src/main/java/edu/icet/service/ItemService.java
+++ src/main/java/edu/icet/service/ItemService.java
@@ -1,4 +1,16 @@
 package edu.icet.service;
 
+import edu.icet.model.dto.ItemDTO;
+
+import java.util.List;
+
 public interface ItemService {
+
+    boolean addItem(ItemDTO item);
+
+    boolean updateItem(ItemDTO item);
+
+    boolean deleteItem(Integer id);
+
+    List<ItemDTO> getAll();
 }
