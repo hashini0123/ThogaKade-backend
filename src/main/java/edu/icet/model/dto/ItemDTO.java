3ce940485046c2723b3af8ebb4b45cd0f3c0cb3d
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-23 22:08:40 +0530

    create customer commit #15

---
+++ src/main/java/edu/icet/model/dto/ItemDTO.java
@@ -0,0 +1,24 @@
+package edu.icet.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class ItemDTO {
+
+    private String itemCode;
+
+    private String description;
+
+    private String packSize;
+
+    private double unitPrice;
+
+    private int qtyOnHand;
+
+}
