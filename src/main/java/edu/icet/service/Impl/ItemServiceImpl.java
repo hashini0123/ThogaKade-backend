ee4285242cf33b04b73c8905d59345c8254fcaad
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-25 01:32:38 +0530

    create customer commit #32

--- src/main/java/edu/icet/service/Impl/ItemServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/ItemServiceImpl.java
@@ -1,28 +1,36 @@
 package edu.icet.service.Impl;
 
 import edu.icet.model.dto.ItemDTO;
+import edu.icet.repository.ItemRepository;
 import edu.icet.service.ItemService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@AllArgsConstructor
+@Service
 public class ItemServiceImpl implements ItemService {
+
+    final private ItemRepository itemRepository;
+
     @Override
     public boolean addItem(ItemDTO item) {
-        return false;
+        return itemRepository.addItem(item);
     }
 
     @Override
     public boolean updateItem(ItemDTO item) {
-        return false;
+        return itemRepository.updateItem(item);
     }
 
     @Override
     public boolean deleteItem(Integer id) {
-        return false;
+        return itemRepository.deleteItem(id);
     }
 
     @Override
     public List<ItemDTO> getAll() {
-        return List.of();
+        return itemRepository.getAll();
     }
 }
