9b0e94f9a1a7b2e84ad5eccb16514e4fc01c58b9
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-25 01:32:11 +0530

    create customer commit #30

--- src/main/java/edu/icet/controller/ItemController.java
+++ src/main/java/edu/icet/controller/ItemController.java
@@ -1,4 +1,38 @@
 package edu.icet.controller;
 
+import edu.icet.model.dto.ItemDTO;
+import edu.icet.service.ItemService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200")
 public class ItemController {
+
+    final private ItemService itemService;
+
+    @PostMapping("/add")
+    public boolean addItem(@RequestBody ItemDTO item){
+        return itemService.addItem(item);
+    }
+
+    @PutMapping("/update")
+    public boolean updateItem(@RequestBody ItemDTO item){
+        return itemService.updateItem(item);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public boolean deleteById(@PathVariable Integer id){
+        return itemService.deleteItem(id);
+    }
+
+    @GetMapping("/get")
+    public List<ItemDTO> getAll(){
+        return itemService.getAll();
+    }
+
+
 }
