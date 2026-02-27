9d7a7ee682b0ea5955c54e6cefe103e738218886
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-28 01:10:40 +0530

    create customer commit #42

--- src/main/java/edu/icet/service/Impl/OrderServiceImpl.java
+++ src/main/java/edu/icet/service/Impl/OrderServiceImpl.java
@@ -1,4 +1,41 @@
 package edu.icet.service.Impl;
 
-public class OrderServiceImpl {
+import edu.icet.model.dto.OrderDTO;
+import edu.icet.repository.OrderRepository;
+import edu.icet.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class OrderServiceImpl implements OrderService {
+
+    final private OrderRepository orderRepository;
+
+    @Override
+    public boolean addOrder(OrderDTO orderDTO) {
+        return orderRepository.addOrder(orderDTO);
+    }
+
+    @Override
+    public boolean updateOrder(OrderDTO orderDTO) {
+        return orderRepository.updateOrder(orderDTO);
+    }
+
+    @Override
+    public boolean deleteById(Integer id) {
+        return orderRepository.deleteById(id);
+    }
+
+    @Override
+    public OrderDTO searchById(Integer id) {
+        return orderRepository.searchById(id);
+    }
+
+    @Override
+    public List<OrderDTO> getAll() {
+        return orderRepository.getAll();
+    }
 }
