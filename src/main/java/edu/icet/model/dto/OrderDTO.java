df4ebed6533687593ebb752f0781db3637d61d30
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-28 01:10:04 +0530

    create customer commit #39

--- src/main/java/edu/icet/model/dto/OrderDTO.java
+++ src/main/java/edu/icet/model/dto/OrderDTO.java
@@ -1,4 +1,21 @@
 package edu.icet.model.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import java.util.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class OrderDTO {
+
+    private String orderID;
+
+    private Date orderDate;
+
+    private String custID;
 }
