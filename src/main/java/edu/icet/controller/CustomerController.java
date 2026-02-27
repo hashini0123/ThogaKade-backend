4034804fce6a809f0f24ff25bb71e0eac8bd6019
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-27 23:25:26 +0530

    create customer commit #33

--- src/main/java/edu/icet/controller/CustomerController.java
+++ src/main/java/edu/icet/controller/CustomerController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequestMapping("/cutomer")
 @RequiredArgsConstructor
 @CrossOrigin(origins = "http://localhost:4200")
 public class CustomerController {
