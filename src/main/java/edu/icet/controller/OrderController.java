4f856989fc046967f4b32838cd00208ab00962a5
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-27 23:54:10 +0530

    create customer commit #36

--- src/main/java/edu/icet/controller/OrderController.java
+++ src/main/java/edu/icet/controller/OrderController.java
@@ -1,16 +1,39 @@
 package edu.icet.controller;
 
 
+import edu.icet.model.dto.OrderDTO;
+import edu.icet.service.OrderService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping
 @CrossOrigin
 @RequiredArgsConstructor
 public class OrderController {
+
+    final private OrderService orderService;
+
+    @PostMapping("/add")
+    public boolean addOrder(@RequestBody OrderDTO orderDTO){
+        orderService.addOrder(orderDTO);
+    }
+
+    @PutMapping("/update")
+    public boolean updateOrder(@RequestBody OrderDTO orderDTO){
+        orderService.updateOrder(orderDTO);
+    }
+
+    @DeleteMapping("/delete-by-id/{id}")
+    public boolean deleteById(@Integer id){
+        orderService.deleteById(id);
+    }
+
+    @GetMapping("/search-by-id/{id}")
+    public OrderDTO searchById(Integer id){
+        return orderService.searchById(id);
+    }
+
 
 
 
