a7488a13704dedc6bc628014db3e3b09cefeb892
hashini0123 <nanayakkarahashini5@gmail.com>
2026-02-27 23:25:57 +0530

    create customer commit #35

--- src/main/java/edu/icet/controller/OrderController.java
+++ src/main/java/edu/icet/controller/OrderController.java
@@ -1,4 +1,18 @@
 package edu.icet.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@CrossOrigin
+@RequiredArgsConstructor
 public class OrderController {
+
+
+
+
 }
